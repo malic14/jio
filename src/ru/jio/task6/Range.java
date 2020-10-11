8119a79f11b33c240add8f692200d2d9d22c1378
lexx <lexx@DESKTOP-V824PHT>
2020-10-11 21:05:51 +0300

    Add task6

---
+++ src/ru/jio/task6/Range.java
@@ -0,0 +1,59 @@
+package ru.jio.task6;
+
+import java.util.*;
+
+public class Range {
+
+	public static void main(String[] args) {
+		//create shooting range (1000x1000)
+		boolean[][] b = new boolean[1000][1000];
+		b[498][500] = true;
+		b[499][500] = true;
+		b[500][500] = true;
+		b[501][500] = true;
+		b[498][501] = true;
+		b[499][501] = true;
+		b[500][501] = true;
+		b[501][501] = true;
+		b[498][502] = true;
+		b[499][502] = true;
+		b[500][502] = true;
+		b[501][502] = true;
+		b[498][503] = true;
+		b[499][503] = true;
+		b[500][503] = true;
+		b[501][503] = true;
+		b[496][499] = true;
+		b[497][499] = true;
+		b[498][499] = true;
+		b[499][499] = true;
+		b[500][499] = true;
+		b[501][499] = true;
+		b[502][499] = true;
+		b[503][499] = true;
+		b[496][498] = true;
+		b[497][498] = true;
+		b[498][498] = true;
+		b[499][498] = true;
+		b[500][498] = true;
+		b[501][498] = true;
+		b[502][498] = true;
+		b[503][498] = true;
+		b[496][497] = true;
+		b[497][497] = true;
+		b[498][497] = true;
+		b[499][497] = true;
+		b[500][497] = true;
+		b[501][497] = true;
+		b[502][497] = true;
+		b[503][497] = true;
+		Scanner sc = new Scanner(System.in);
+		System.out.print(">> ");
+		int x = sc.nextInt()+499;
+		System.out.print(">> ");
+		int y = sc.nextInt()+499;
+		System.out.print(b[x][y]);
+		sc.close();
+	}
+
+}
